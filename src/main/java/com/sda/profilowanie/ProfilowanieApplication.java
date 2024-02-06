1c2e6cbd1d0c59aa54de610a8420d79b420b0638
shantais <waczynska.a@gmail.com>
2024-02-06 12:55:00 +0100

    SDA project - creating profiles for spring projects

---
+++ src/main/java/com/sda/profilowanie/ProfilowanieApplication.java
@@ -0,0 +1,13 @@
+package com.sda.profilowanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilowanieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilowanieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/profilowanie/ProfilowanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.profilowanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilowanieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
